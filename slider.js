9c31f3ef2cad3c725c717fe7da66ef78e5dfad72
Joe <josephdbonham@googlemail.com>
2013-10-17 13:46:14 +0100

    no message

--- slider.js
+++ slider.js
@@ -15,16 +15,16 @@
 						
 		return this.each( function() {
 		
-			var full	=	$(this),
-			inner		=	full.find('.inner'),
-			slides		=	inner.find('.slide'),
-			images		=	slides.find('img'),
-			nav		=	full.find('.slide-nav'),
-			controls	=	full.find('.controls a'),
-			navCircles	=	'',
-			smallest	=	9999,
-			status		=	{current : 0, max : slides.length - 1},
-			timers		=	{slides : '', resize : ''},
+			var full		=	$(this),
+			inner			=	full.find('.inner'),
+			slides			=	inner.find('.slide'),
+			images			=	slides.find('img'),
+			nav				=	full.find('.slide-nav'),
+			controls		=	full.find('.controls a'),
+			navCircles		=	'',
+			smallest		=	9999,
+			status			=	{current : 0, max : slides.length - 1},
+			timers			=	{slides : '', resize : ''},
 			move = function(direction, current){
 				if(inner.is(':animated')) return;
 			
@@ -50,12 +50,12 @@
 			},
 			resize = function(){
 				
-				var wWidth	=	$(window).width(),
-				newHeight	=	parseInt(wWidth/3, 10),
-				imageCSS	=	wWidth <= smallest ? ['100%', 'auto', '9999'] : ['', '', ''],
-				start		=	inner.height(),
-				divCSS		=	wWidth <= 480 ? ['0', '100%', 'none'] : ['', '', ''],
-				size		=	wWidth/41;
+				var wWidth		=	$(window).width(),
+				newHeight		=	parseInt(wWidth/3, 10),
+				imageCSS		=	wWidth <= smallest ? ['100%', 'auto', '9999'] : ['', '', ''],
+				start			=	inner.height(),
+				divCSS			=	wWidth <= 480 ? ['0', '100%', 'none'] : ['', '', ''],
+				size			=	wWidth/41;
 					
 				size = size > settings.maxFont ? settings.maxFont : 
 							(size < settings.minFont ? settings.minFont : size);
