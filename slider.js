528aa5133528eca2bd2f7136ed7aca520e9c7d9d
Joe <josephdbonham@googlemail.com>
2013-10-17 13:55:56 +0100

    no message

--- slider.js
+++ slider.js
@@ -1,136 +1,114 @@
-(function($){ 
+(function($){
 	
 	"use strict";
 	
-	$.fn.fullWidth = function(options) {
+	$.fn.fullWidth = function(options){
 			
 		var defaults = {
-			maxHeight 	:	450,
-			minHeight 	:	375,
-			delay	 		:	5000,
-			transition	:	1000,
-			maxFont	 	:	36,
-			minFont	 	:	20
+			maxHeight		:	450,
+			minHeight		:	375,
+			delay			:	5000,
+			transition		:	1000,
+			maxFont			:	36,
+			minFont			:	20
 		}, settings = $.extend(defaults, options);
 						
-		return this.each( function() {
-		
-			var full		=	$(this),
-<<<<<<< HEAD
-			inner			=	full.find('.inner'),
-			slides			=	inner.find('.slide'),
-			images			=	slides.find('img'),
-			nav				=	full.find('.slide-nav'),
-			controls		=	full.find('.controls a'),
-			navCircles		=	'',
-			smallest		=	9999,
-			status			=	{current : 0, max : slides.length - 1},
-			timers			=	{slides : '', resize : ''},
-=======
-			inner			 =	full.find('.inner'),
-			slides			=	inner.find('.slide'),
-			images			=	slides.find('img'),
-			nav	  	 	=	full.find('.slide-nav'),
-			controls		=	full.find('.controls a'),
-			navCircles	=	'',
-			smallest  	=	9999,
-			status	  	=	{current : 0, max : slides.length - 1},
-			timers	  	=	{slides : '', resize : ''},
->>>>>>> d929909c588a43391686c0b642397417e97cdc37
-			move = function(direction, current){
-				if(inner.is(':animated')) return;
+		return this.each( function(){
 			
-				stop();
+			var full			=	$(this),
+				inner			=	full.find('.inner'),
+				slides			=	inner.find('.slide'),
+				images			=	slides.find('img'),
+				nav				=	full.find('.slide-nav'),
+				controls		=	full.find('.controls a'),
+				navCircles		=	'',
+				smallest		=	9999,
+				status			=	{current : 0, max : slides.length - 1},
+				timers			=	{slides : '', resize : ''},
+				move = function(direction, current) {
+					if(inner.is(':animated')) return;
 			
-				if(direction === 'right'){
-					status.current = status.current+1 > status.max ? 0 : status.current+1;
-				}else if(direction === 'left'){
-					status.current = status.current-1 < 0 ? status.max : status.current-1;
-				}else{
-					status.current = current || "0";
-				}
+					stop();
 			
-				navCircles.removeClass('current').eq(status.current).addClass('current');
-				inner.animate({'margin-left' : '-'+100*status.current+'%'}, settings.transition ,function(){ start(); });
+					if(direction === 'right'){
+						status.current = status.current+1 > status.max ? 0 : status.current+1;
+					}else if(direction === 'left'){
+						status.current = status.current-1 < 0 ? status.max : status.current-1;
+					}else{
+						status.current = current || "0";
+					}
 			
-			},
-			start = function(){
-				timers.slides = setTimeout(function(){ move('right'); }, settings.delay);
-			},
-			stop = function(){
-				clearTimeout(timers.slides);
-			},
-			resize = function(){
+					navCircles.removeClass('current').eq(status.current).addClass('current');
+					inner.animate({'margin-left' : '-'+100*status.current+'%'}, settings.transition ,function() { start(); });
+			
+				},
+				start = function() {
+					timers.slides = setTimeout(function(){ move('right'); }, settings.delay);
+				},
+				stop = function() {
+					clearTimeout(timers.slides);
+				},
+				resize = function() {
 				
-<<<<<<< HEAD
-				var wWidth		=	$(window).width(),
-				newHeight		=	parseInt(wWidth/3, 10),
-				imageCSS		=	wWidth <= smallest ? ['100%', 'auto', '9999'] : ['', '', ''],
-				start			=	inner.height(),
-				divCSS			=	wWidth <= 480 ? ['0', '100%', 'none'] : ['', '', ''],
-				size			=	wWidth/41;
-=======
-				var wWidth	=	$(window).width(),
-				newHeight 	=	parseInt(wWidth/3, 10),
-				imageCSS  	=	wWidth <= smallest ? ['100%', 'auto', '9999'] : ['', '', ''],
-				start 			=	inner.height(),
-				divCSS	  	=	wWidth <= 480 ? ['0', '100%', 'none'] : ['', '', ''],
-				size  			=	wWidth/41;
->>>>>>> d929909c588a43391686c0b642397417e97cdc37
-					
-				size = size > settings.maxFont ? settings.maxFont : 
+					var wWidth		=	$(window).width(),
+						newHeight		=	parseInt(wWidth/3, 10),
+						imageCSS		=	wWidth <= smallest ? ['100%', 'auto', '9999'] : ['', '', ''],
+						start			=	inner.height(),
+						divCSS			=	wWidth <= 480 ? ['0', '100%', 'none'] : ['', '', ''],
+						size			=	wWidth/41;
+						size = size > settings.maxFont ? settings.maxFont : 
 							(size < settings.minFont ? settings.minFont : size);
 
-				inner.css('height', function(){
-					return newHeight > settings.maxHeight ? settings.maxHeight : 
-							(newHeight < settings.minHeight ? settings.minHeight : newHeight);
-				});
+					inner.css('height', function(){
+						return newHeight > settings.maxHeight ? settings.maxHeight : 
+									(newHeight < settings.minHeight ? settings.minHeight : newHeight);
+					});
 
-				images.css({
-					'margin-top' : function(){
-						var curr = $(this).height();
-						return '-'+(start > curr ? 0 : curr-start)/2+'px';
-					},
-					'height' : imageCSS[0], 'width' : imageCSS[1], 'maxWidth' : imageCSS[2]
-				});
+					images.css({
+						'margin-top' : function(){
+							var curr = $(this).height();
+							return '-'+(start > curr ? 0 : curr-start)/2+'px';
+						},
+						'height' : imageCSS[0], 'width' : imageCSS[1], 'maxWidth' : imageCSS[2]
+					});
 
-				slides.find('div').css({
-					'font-size' : size, 
-					'top' : function(){
-						var diff = start-$(this).height();
-						return wWidth <= 480 ? diff : diff/2;
-					},
-					'padding' : divCSS[0], 'width' : divCSS[1]
-				}).find('br').css('display', divCSS[2]);
+					slides.find('div').css({
+						'font-size' : size, 
+						'top' : function(){
+							var diff = start-$(this).height();
+							return wWidth <= 480 ? diff : diff/2;
+						},
+						'padding' : divCSS[0], 'width' : divCSS[1]
+					}).find('br').css('display', divCSS[2]);
 				
-			},
-			attachEvents = function(){
+				},
+				attachEvents = function(){
 			
-				$(window).resize(function(){
-					clearTimeout(timers.resize);
-					timers.resize = setTimeout(function(){ resize(); }, 100);
-				}).trigger('resize');
+					$(window).resize(function(){
+						clearTimeout(timers.resize);
+						timers.resize = setTimeout(function(){ resize(); }, 100);
+					}).trigger('resize');
 				
-				controls.on('click', function(){
-					move(this.className);
-					return false;
-				});
+					controls.on('click', function(){
+						move(this.className);
+						return false;
+					});
 				
-				full.on('mouseenter mouseleave', function(e){
-					if(controls.is(':animated')) return;
-					e.type === 'mouseenter' ? controls.fadeIn() : controls.fadeOut();
-				});
+					full.on('mouseenter mouseleave', function(e){
+						if(controls.is(':animated')) return;
+						e.type === 'mouseenter' ? controls.fadeIn() : controls.fadeOut();
+					});
 				
-				navCircles.on('click', function(){
-					move('direct', $(this).index());
-				});
+					navCircles.on('click', function(){
+						move('direct', $(this).index());
+					});
 				
-				$(document).on('keydown', function(e){
-					if(!(e.which === 37 || e.which === 39)) return; 
-					move(e.which === 37 ? 'left' : 'right');
-				});
+					$(document).on('keydown', function(e){
+						if(!(e.which === 37 || e.which === 39)) return; 
+						move(e.which === 37 ? 'left' : 'right');
+					});
 	
-			};
+				};
 		
 			(function(){
 				
